ab5aaa2e2014c53645f7ce9e857601abab07cdae
AugustusTheBirb <augustasrink@gmail.com>
2025-02-01 17:18:32 +0200

    exercises 04-07

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    for (let i = 1; i<arguments.length; i++ ){
+        spliceLocation = arr.indexOf(arguments[i])
+        while ( spliceLocation >= 0) {
+            arr.splice(spliceLocation, 1)
+            spliceLocation = arr.indexOf(arguments[i])
+        }
+    }
+    return arr
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    let s = 0
+    if (Math.floor(a) !== a || Math.floor(b) !== b) {return "ERROR"}
+    if (typeof(a) !== "number" || typeof(b) !== "number") {return "ERROR"}
+    m = Math.min(a,b)
+    M = Math.max(a,b)
+    if (m < 0){return "ERROR"}
+    for (let i = m; i < M + 1; i++){
+        s += i
+    }
+    return s
 };
 
 // Do not edit below this line
